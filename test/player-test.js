d5e380f93450af148ad9c117e0c313e4c5803491
Veronica Andrade <veronica726@gmail.com>
2019-12-16 13:31:20 -0700

    Adjust test for player class for multiple players

--- test/player-test.js
+++ test/player-test.js
@@ -2,31 +2,36 @@ import chai from 'chai';
 const expect = chai.expect;
 
 const Player = require('../src/Player')
+const Game = require('../src/Game')
 
 describe('Player', () => {
-  let player;
+  let player1, player2, player3;
+  let game
   
   beforeEach(() => {
-    player = new Player('Billy')
+    player1 = new Player('Carla')
+    player2 = new Player('Vee')
+    player3 = new Player('Novak')
+    game = new Game([player1, player2, player3])
   });
 
   it('should be an instance of a player', () => {
-    expect(player).to.be.an.instanceOf(Player)
+    expect(player1).to.be.an.instanceOf(Player)
   });
 
   it('should have a name', () => {
-    expect(player.name).to.equal('Billy')
+    expect(player1.name).to.equal('Carla')
   });
 
   it('should know whether it\'s their turn or not', () => {
-    expect(player.turn).to.equal(false)
+    expect(player1.turn).to.equal(false)
   });
    
   it('should have a starting score of 0', () => {
-    expect(player.score).to.equal(0)
+    expect(player1.score).to.equal(0)
   });
 
   // it('should be able to start a new game', () => {
-  //   expect(player.startGame()).to.equal()
+  //   expect(player1.startGame([player1, player2, player3])).to.equal(game)
   // });
 });
