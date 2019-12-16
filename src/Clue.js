35152e93223876e00b3dd13d95812a861b05868c
Ryan Novak <ryan.novak95@yahoo.com>
2019-12-16 10:03:43 -0700

    Wrote basic methods and test for Clue class

--- src/Clue.js
+++ src/Clue.js
@@ -1,6 +1,19 @@
 class Clue {
-    constructor() {
-        
+    constructor(clue) {
+        this.question = clue.question;
+        this.pointValue = clue.pointValue;
+        this.answer= clue.answer;
+        this.categoryId = clue.categoryId;
+        this.dailyDouble = false;
+        this.alreadySelected = false;
+    }
+
+    activateDailyDouble() {
+        this.dailyDouble = true;
+    }
+
+    selectCard() {
+        this.alreadySelected = true;
     }
 
 }
--- test/Clue-test.js
+++ test/Clue-test.js
@@ -3,7 +3,11 @@ const expect = chai.expect;
 const Clue = require('../src/clue');
 
 describe('Clue', function () {
-  let clue = new Clue();
+    let question = {question: "Scorecard Report\" & \"Peter Jacobsen Plugged In\" are seen on the sports channel devoted to this",
+    pointValue: 100,
+    answer: "golf",
+    categoryId: 10}
+    let clue = new Clue(question);
 
 
     it('is a function', function () {
@@ -13,5 +17,29 @@ describe('Clue', function () {
     it('should be an instance of Clue', function () {
         expect(clue).to.be.an.instanceof(Clue);
     });
+
+    it('should have a question, point value, correct answer, and category ID', function () {
+        expect(clue.question).to.equal("Scorecard Report\" & \"Peter Jacobsen Plugged In\" are seen on the sports channel devoted to this");
+        expect(clue.pointValue).to.equal(100);
+        expect(clue.answer).to.equal("golf");
+        expect(clue.categoryId).to.equal(10);
+    });
+
+    it('should have Daily Double and already selected default to false', function () {
+        expect(clue.dailyDouble).to.equal(false);
+        expect(clue.alreadySelected).to.equal(false);
+    });
+
+    it('should be able to be a Daily Double clue', function() {
+        clue.activateDailyDouble();
+
+        expect(clue.dailyDouble).to.equal(true);
+    });
+
+    it('should toggle its already selectd value once selected', function() {
+        clue.selectCard();
+
+        expect(clue.alreadySelected).to.equal(true);
+    })
 
 });
