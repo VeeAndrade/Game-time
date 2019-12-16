3bc40482d9369cf06ffdf458b2846aaa902ebe52
Veronica Andrade <veronica726@gmail.com>
2019-12-16 13:31:03 -0700

    Sketch out  one method for starting game

--- src/Player.js
+++ src/Player.js
@@ -1,9 +1,15 @@
+const Game = require('../src/Game')
 class Player {
   constructor(name) {
     this.name = name,
     this.turn = false,
     this.score = 0
   }
+
+  // startGame(playersArr) {
+  //   game = new Game(playersArr)
+  //   return game;
+  // }
 }
 
 module.exports = Player;
