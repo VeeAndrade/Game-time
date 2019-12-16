b379ad6d86cc57c9375447c35d9e8d7ae5f38dce
Veronica Andrade <veronica726@gmail.com>
2019-12-16 13:08:18 -0700

    Merging base player test into master

---
+++ test/Player-test.js
@@ -0,0 +1,33 @@
+import chai from 'chai';
+const expect = chai.expect;
+
+const Player = require('../src/Player')
+
+
+describe('Player', () => {
+  let player;
+  
+  beforeEach(() => {
+    player = new Player('Billy')
+  });
+
+  it('should be an instance of a player', () => {
+    expect(player).to.be.an.instanceOf(Player)
+  });
+
+  it('should have a name', () => {
+    expect(player.name).to.equal('Billy')
+  });
+
+  it('should know whether it\'s their turn or not', () => {
+    expect(player.turn).to.equal(false)
+  });
+   
+  it('should have a starting score of 0', () => {
+    expect(player.score).to.equal(0)
+  });
+
+  // it('should be able to start a new game', () => {
+  //   expect(player.startGame()).to.equal()
+  // });
+});
