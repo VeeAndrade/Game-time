f2c52b60d5517fe30205b451c51aac0699e03e7f
Carla Geglio <carla.geglio@gmail.com>
2019-12-16 19:47:30 -0700

    Instantiate a new game when play button is clicked

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ import './css/base.scss';
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
 import Player from '../src/Player'
+import Game from '../src/Game'
 
 let nameInputs = document.querySelector(".player-name-input");
 let gameRules = document.querySelector(".game-rules");
@@ -22,10 +23,13 @@ let player3Name = document.querySelector(".player3-name");
 let continueBtn = document.querySelector(".continue-button");
 let gameBoard = document.querySelector(".game-board");
 let playBtn = document.querySelector(".play-button");
+let player1;
+let player2;
+let player3;
 
 nameInputs.addEventListener("keyup", checkInputs);
 continueBtn.addEventListener("click", instantiatePlayers);
-playBtn.addEventListener("click", showGame);
+playBtn.addEventListener("click", instantiateGame);
 
 function checkInputs() {
   if (player1Input.value && player2Input.value && player3Input.value) {
@@ -35,9 +39,9 @@ function checkInputs() {
 
 function instantiatePlayers() {
   if (continueBtn.id === "active") {
-    let player1 = new Player(player1Input.value);
-    let player2 = new Player(player2Input.value);
-    let player3 = new Player(player3Input.value);
+    player1 = new Player(player1Input.value);
+    player2 = new Player(player2Input.value);
+    player3 = new Player(player3Input.value);
     showRules();
   } else {
     document.querySelector(".error").style.display = "block";
@@ -50,6 +54,11 @@ function showRules() {
   nameInputs.style.display = "none";
   gameRules.style.display = "block";
 };
+
+function instantiateGame() {
+  let game = new Game([player1, player2, player3]);
+  showGame();
+}
 
 function showGame() {
   player1Name.innerText = `${player1Input.value}`;
