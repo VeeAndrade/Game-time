11248c16f3e0a8884d8599070cb6bc9660030268
Veronica Andrade <veronica726@gmail.com>
2019-12-17 13:34:59 -0700

    merge resolve

--- src/index.js
+++ src/index.js
@@ -9,12 +9,27 @@ import './css/base.scss';
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
+
 import Clue from '../src/Clue'
-import '../src/Game'
-import '../src/Player'
+import Player from '../src/Player'
+import Game from '../src/Game'
 import '../src/Round'
 // import { promises } from 'dns';
 
+let nameInputs = document.querySelector(".player-name-input");
+let gameRules = document.querySelector(".game-rules");
+let player1Input = document.querySelector(".player1");
+let player2Input = document.querySelector(".player2");
+let player3Input = document.querySelector(".player3");
+let player1Name = document.querySelector(".player1-name");
+let player2Name = document.querySelector(".player2-name");
+let player3Name = document.querySelector(".player3-name");
+let continueBtn = document.querySelector(".continue-button");
+let gameBoard = document.querySelector(".game-board");
+let playBtn = document.querySelector(".play-button");
+let player1;
+let player2;
+let player3;
 let clue;
 let clueCategories = [];
 let clueInfo = [];
@@ -48,7 +63,6 @@ function getFetches() {
   return Promise.all([clueFetch(), categoryFetch()])
 }
 
-
 function instantiateClues() {
   return clueInfo.map(c => { 
     clue = new Clue(c)
@@ -59,8 +73,47 @@ function instantiateClues() {
 getFetches()
 .then(() => instantiateClues())
 
-
 instantiateClues()
-  
 
-  
+
+nameInputs.addEventListener("keyup", checkInputs);
+continueBtn.addEventListener("click", instantiatePlayers);
+playBtn.addEventListener("click", instantiateGame);
+
+function checkInputs() {
+  if (player1Input.value && player2Input.value && player3Input.value) {
+    continueBtn.id = "active";
+  }
+};
+
+function instantiatePlayers() {
+  if (continueBtn.id === "active") {
+    player1 = new Player(player1Input.value);
+    player2 = new Player(player2Input.value);
+    player3 = new Player(player3Input.value);
+    showRules();
+  } else {
+    document.querySelector(".error").style.display = "block";
+  }
+};
+
+function showRules() {
+  let welcomeMsg = `<h3>Welcome ${player1Input.value}, ${player2Input.value}, & ${player3Input.value}!`
+  gameRules.insertAdjacentHTML("afterbegin", welcomeMsg);
+  nameInputs.style.display = "none";
+  gameRules.style.display = "block";
+};
+
+function instantiateGame() {
+  let game = new Game([player1, player2, player3]);
+  showGame();
+}
+
+function showGame() {
+  player1Name.innerText = `${player1Input.value}`;
+  player2Name.innerText = `${player2Input.value}`;
+  player3Name.innerText = `${player3Input.value}`;
+  gameRules.style.display = "none";
+  gameBoard.style.display = "grid";
+}
+
