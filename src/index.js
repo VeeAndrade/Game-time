7092a8ad33289ec5f3d913d71c5d4f3feec86e05
Carla Geglio <carla.geglio@gmail.com>
2019-12-16 19:22:03 -0700

    Instantiate new players and put names on the dom when continue is clicked

--- src/index.js
+++ src/index.js
@@ -9,5 +9,35 @@ import './css/base.scss';
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
+import Player from '../src/Player'
 
-console.log('This is the JavaScript entry file - your code begins here.');
+let nameInputs = document.querySelector(".player-name-input");
+let gameRules = document.querySelector(".game-rules");
+let player1Name = document.querySelector(".player1");
+let player2Name = document.querySelector(".player2");
+let player3Name = document.querySelector(".player3");
+let continueBtn = document.querySelector(".continue-button");
+
+nameInputs.addEventListener("keyup", checkInputs);
+continueBtn.addEventListener("click", showDirections);
+
+function checkInputs() {
+  if (player1Name.value && player2Name.value && player3Name.value) {
+    continueBtn.id = "active";
+  }
+};
+
+function showDirections() {
+  if (continueBtn.id === "active") {
+    let player1 = new Player(player1Name.value);
+    let player2 = new Player(player2Name.value);
+    let player3 = new Player(player3Name.value);
+    nameInputs.style.display = "none";
+    gameRules.style.display = "block";
+    let welcomeMsg = `<h3>Welcome ${player1Name.value}, ${player2Name.value}, & ${player3Name.value}!`
+    gameRules.insertAdjacentHTML("afterbegin",
+    welcomeMsg);
+  } else {
+    document.querySelector(".error").style.display = "block";
+  }
+};
